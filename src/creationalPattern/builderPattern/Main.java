5fff960ddcc78c3f808abceb968c8a20076f81e0
korbuno <aileen528@naver.com>
2018-07-22 16:23:14 +0900

    no message

--- src/creationalPattern/builderPattern/Main.java
+++ src/creationalPattern/builderPattern/Main.java
@@ -2,14 +2,14 @@ package creationalPattern.builderPattern;
 
 public class Main {
 	public static void main(String[] args) {
-		PersonBuilder personBuilder = new PersonBuilder();
 		
-		Person person = personBuilder
+		Person person = new PersonBuilder()
 				.setAge(27)
 				.setGender("남자")
 				.setHeight(179)
 				.setName("윤현구")
 				.build();
 		System.out.println(person);
+		
 	}
 }
